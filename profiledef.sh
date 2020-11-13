a55e0d4ae8b84ddee5ba8cfeae475f75bfc93d1c
Staubgeborener <eric.schroeder@protonmail.com>
2020-11-13 06:59:17 +0100

    update profiledef.sh

--- profiledef.sh
+++ profiledef.sh
@@ -1,12 +1,12 @@
 #!/usr/bin/env bash
 # shellcheck disable=SC2034
 
-iso_name="archlinux"
-iso_label="ARCH_$(date +%Y%m)"
-iso_publisher="Arch Linux <https://www.archlinux.org>"
-iso_application="Arch Linux Live/Rescue CD"
+iso_name="gatelinux"
+iso_label="GATE_$(date +%Y%m)"
+iso_publisher="Gate Linux <https://www.github.com/gate-os/>"
+iso_application="Gate Linux Live CD"
 iso_version="$(date +%Y.%m.%d)"
-install_dir="arch"
+install_dir="gate"
 bootmodes=('bios.syslinux.mbr' 'bios.syslinux.eltorito' 'uefi-x64.systemd-boot.esp' 'uefi-x64.systemd-boot.eltorito')
 arch="x86_64"
 pacman_conf="pacman.conf"
